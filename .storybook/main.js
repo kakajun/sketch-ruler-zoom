06c134905709874882e319df49e648c26567bbc2
马军 <majun@xiwicloud.com>
2023-12-31 23:05:32 +0800

    1

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,8 +7,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    '@storybook/addon-storysource',
-    '@storybook/addon-docs',
+    '@storybook/addon-storysource'
   ],
   framework: {
     name: "@storybook/vue3-vite",
