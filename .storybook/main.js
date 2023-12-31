e514e1a58dcf34d9565e36d87b34477a691c46b7
马军 <majun@xiwicloud.com>
2023-12-31 22:52:53 +0800

    init

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx",
+   "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-storysource',
+    '@storybook/addon-docs',
   ],
   framework: {
     name: "@storybook/vue3-vite",
